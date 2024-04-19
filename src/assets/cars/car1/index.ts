46a935e602fb763bb55ee4f1135a203a0db09284
Gabriel Oliveira <g4brieloliveira.dev@gmail.com>
2024-04-19 10:30:30 -0300

    ✨ feat: add car1 assets

---
+++ src/assets/cars/car1/index.ts
@@ -0,0 +1,10 @@
+export const car1 = {
+  north: require("./north.png"),
+  northeast: require("./northeast.png"),
+  east: require("./east.png"),
+  southeast: require("./southeast.png"),
+  south: require("./south.png"),
+  southwest: require("./southwest.png"),  
+  west: require("./west.png"),
+  northwest: require("./northwest.png"),
+};
